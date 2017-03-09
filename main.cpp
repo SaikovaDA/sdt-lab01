943e5c7d61032e6b860c36d257c89549bfa35aeb
Дарья Сайкова <saikova1998@mail.ru>
2017-03-09 17:08:30 +0300

    Mult

--- main.cpp
+++ main.cpp
@@ -6,6 +6,6 @@ int main()
     int a,b;
     cout<<"Enter A and B:"
             cin>>a>>b;
-    cout<<"A+B="<<a+b<<'\n';
-    cout<<"A*2="<<a*2<<'\n';
+    cout<<"A+B ="<<a+b<<'\n';
+    cout<<"A*2 ="<<a*2<<'\n';
 }
